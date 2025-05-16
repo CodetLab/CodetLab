7dd000de758559814ba697909c8b6da628b7f6b8
Giovanni Peirone <mgpeirone25@gmail.com>
2025-05-16 19:43:53 -0300

    asd

--- web-client/src/app/pages/home/home-sections/about-us/about-us.component.ts
+++ web-client/src/app/pages/home/home-sections/about-us/about-us.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  
+  slide = 1;
+  SlideMaximo = 3
+  SlideMinimo = 1
+
+  slidePlus1(){
+    console.log(this.slide)
+    if (this.slide == this.SlideMaximo){
+      this.slide = this.SlideMinimo
+    }
+    else {
+      this.slide += 1
+    }
+
+  }
+
+  slideMinus1(){
+    console.log(this.slide)
+    if (this.slide == this.SlideMinimo){
+      this.slide = this.SlideMaximo
+    }
+    else {
+      this.slide -= 1
+    }
+  }
 
 }
