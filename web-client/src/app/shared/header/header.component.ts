96adeeb97a03104d5ba23615e2926c2c07c5cc92
Giovanni Peirone <mgpeirone25@gmail.com>
2025-05-15 21:03:58 -0300

    hi
    header (Listo)
    init (listo)

--- web-client/src/app/shared/header/header.component.ts
+++ web-client/src/app/shared/header/header.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  viewHeader() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
